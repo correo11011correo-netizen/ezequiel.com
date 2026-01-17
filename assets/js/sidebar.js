50de411653c85324bc784a5b0fdd57d06ffca093
correo11011correo-netizen <correo11011correo@gmail.com>
2026-01-16 21:33:50 -0300

    Create sidebar.js

---
+++ assets/js/sidebar.js
@@ -0,0 +1,11 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+menuToggle.addEventListener("click", () => {
+  sidebar.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
